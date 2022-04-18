2fa94dc95fef45fba774e0024a3bdd2989132857
Guest400123064 <lgfz1003@icloud.com>
2022-04-17 23:07:00 -0400

    .

--- src/gen_desc.py
+++ src/gen_desc.py
@@ -16,7 +16,7 @@ logging.basicConfig(
 )
 
 KEY_BASE = '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'
-KEY_REND = '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'
+KEY_REND = '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'
 MDL_BASE = 'ada:ft-cis-700-31-2022-04-10-23-27-31'
 MDL_REND = 'ada:ft-cis-700-14-2022-04-11-02-46-54'
 
